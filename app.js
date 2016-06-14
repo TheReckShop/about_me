e4bbc3eacca7b237ea0ac2db70c218e0610f0b3b
David Cole <thereckshop@mac.com>
2016-06-14 14:57:34 -0700

    more questions

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 var userName = prompt('Greetings!!! What is your name?');
 alert('Welcome' + userName + ', glad you\'re here!');
 
-var answer1 = prompt('Does Sam have 3 cats?');
-if (answer1.toLowerCase() === 'Yes') {
+var answer1 = prompt('Does Sam have 3 cats?').toLowerCase();
+if (answer1() === 'Yes' || answer1() === 'y') {
   alert('Right!');
 } else {
   alert(wrong);
