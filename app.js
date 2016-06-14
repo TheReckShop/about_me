235f7bb0a788b783822a67abbd6ec7f160d3c7e6
David Cole <thereckshop@mac.com>
2016-06-14 15:44:14 -0700

    final draft 2

--- app.js
+++ app.js
@@ -3,35 +3,35 @@ var userName = prompt('Greetings!!! What is your name?');
 alert('Welcome' + userName + ', glad you\'re here!');
 
 var answer1 = prompt('Was I born in Kirkland, WA?').toLowerCase();
-if (answer1 === 'Yes' || answer1 === 'y') {
+if (answer1 === 'yes' || answer1 === 'y') {
   alert('Right! Well done.');
 } else {
   alert('Wrong! Try again...');
 }
 
 var answer2 = prompt('Have I been playing guitar since I was 15?').toLowerCase();
-if (answer2 === 'Yes' || answer2 === 'Y') {
+if (answer2 === 'yes' || answer2 === 'y') {
   alert('Correct! Well done!');
 } else {
   alert('WRONG! How could you not know?');
 }
 
 var answer3 = prompt('Do I have 2 cats?').toLowerCase();
-if (answer3 === 'Yes' || answer3 === 'Y') {
+if (answer3 === 'yes' || answer3 === 'y') {
   alert('Correct! Well done!');
 } else {
   alert('WRONG!');
 }
 
 var answer4 = prompt('Do I hate the Sounders?').toLowerCase();
-if (answer4 === 'No' || answer4 === 'N') {
+if (answer4 === 'no' || answer4 === 'n') {
   alert('Correct! GO SOUNDERS!');
 } else {
   alert('WRONG!');
 }
 
 var answer5 = prompt('Do I manage a night club?').toLowerCase();
-if (answer5 === 'Yes' || answer5 === 'Y') {
+if (answer5 === 'yes' || answer5 === 'y') {
   alert('Correct! I manage the Underground!');
 } else {
   alert('WRONG!');
