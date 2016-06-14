cba6ee91e1cb94acd3f687c728f4a8146f252720
David Cole <thereckshop@mac.com>
2016-06-14 14:44:03 -0700

    added question format

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 'use strict';
 var userName = prompt('Greetings!!! What is your name?');
 alert('Welcome' + userName + ', glad you\'re here!');
+
+var answer1 = prompt('Does Sam have 3 cats?');
+if (answer1.toLowerCase() === 'Yes') {
+  alert('Right!');
+} else {
+  alert(wrong);
+}
