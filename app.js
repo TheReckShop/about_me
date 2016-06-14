eab776db7b8a1a1871bf61e9ad3800fdaaff6df5
David Cole <thereckshop@mac.com>
2016-06-14 15:32:23 -0700

    final draft day 2

--- app.js
+++ app.js
@@ -2,9 +2,37 @@
 var userName = prompt('Greetings!!! What is your name?');
 alert('Welcome' + userName + ', glad you\'re here!');
 
-var answer1 = prompt('Does Sam have 3 cats?').toLowerCase();
-if (answer1() === 'Yes' || answer1() === 'y') {
-  alert('Right!');
+var answer1 = prompt('Was I born in Kirkland, WA?').toLowerCase();
+if (answer1 === 'Yes' || answer1 === 'y') {
+  alert('Right! Well done.');
 } else {
-  alert(wrong);
+  alert('Wrong! Try again...');
+}
+
+var answer2 = prompt('Have I been playing guitar since I was 15?').toLowerCase();
+if (answer2 === 'Yes' || answer2 === 'Y') {
+  alert('Correct! Well done!');
+} else {
+  alert('WRONG! How could you not know?');
+}
+
+var answer3 = prompt('Do I have 2 cats?').toLowerCase();
+if (answer3 === 'Yes' || answer3 === 'Y') {
+  alert('Correct! Well done!');
+} else {
+  alert('WRONG!');
+}
+
+var answer4 = prompt('Do I hate the Sounders?').toLowerCase();
+if (answer4 === 'No' || answer4 === 'N') {
+  alert('Correct! GO SOUNDERS!');
+} else {
+  alert('WRONG!');
+}
+
+var answer5 = prompt('Do I manage a night club?').toLowerCase();
+if (answer5 === 'Yes' || answer5 === 'Y') {
+  alert('Correct! I manage the Underground!');
+} else {
+  alert('WRONG!');
 }
