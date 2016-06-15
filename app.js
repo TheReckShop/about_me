b3347f26cc5879dd7f2648fd539ec6a9e2cbe686
David Cole <thereckshop@mac.com>
2016-06-15 15:25:30 -0700

    question 6 done

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 var userName = prompt('Greetings!!! What is your name?');
-alert('Welcome' + userName + ', glad you\'re here!');
+alert('Welcome ' + userName + ', glad you\'re here!');
 
-var answer1 = prompt('Was I born in Kirkland, WA?').toLowerCase();
+/*var answer1 = prompt('Was I born in Kirkland, WA?').toLowerCase();
 if (answer1 === 'yes' || answer1 === 'y') {
   alert('Right! Well done.');
 } else {
@@ -36,3 +36,33 @@ if (answer5 === 'yes' || answer5 === 'y') {
 } else {
   alert('WRONG!');
 }
+*/
+/*
+var numberGameGuesses = 0;
+while (numberGameGuesses < 4) {
+  var answer6 = parseInt(prompt('What is my favorite number?'));
+  if (answer6 === 7) {
+    alert('Correct! You are obviously strong with the force young padawan...');
+    break;
+  } else if (answer6 < 7) {
+    alert('Wrong! You\'re answer is too high!');
+    numberGameGuesses++;
+  } else if (answer6 > 7) {
+    alert('You\'re answer is too low!');
+    numberGameGuesses++;
+  } else {
+    alert('Please enter a number!');
+    numberGameGuesses++;
+  }
+}
+*/
+var statesLivedIn = [AZ, LA, NY];
+var statesGuesses = 6;
+while (statesGuesses < 6) {
+  var answer7 = statesLivedIn[1] || statesLivedIn[2] || statesLivedIn[3].toUpperCase();
+    alert('Correct! I have lived in that state!');
+    break;
+  } else {
+    alert('Wrong! Please try again!')
+    statesGuesses++;
+  }
