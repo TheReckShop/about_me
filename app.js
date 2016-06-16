47ea787f9e867685e06390f94de5afe955a24d09
Sean Thomas Anderson <Anderson.Sean27@gmail.com>
2016-06-16 13:29:02 -0700

    6th Function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+/*use strict';
 var userName = prompt('Greetings!!! What is your name?');
 alert('Welcome ' + userName + ', glad you\'re here!');
 
@@ -11,7 +11,9 @@ function q1 () {var answer1 = prompt('Was I born in Kirkland, WA?').toLowerCase(
     alert('Wrong! Try again...');
   }
 }
+
 q1();
+
 function q2() { var answer2 = prompt('Have I been playing guitar since I was 15?').toLowerCase();
   if (answer2 === 'yes' || answer2 === 'y') {
     alert('Correct! Well done!');
@@ -20,7 +22,9 @@ function q2() { var answer2 = prompt('Have I been playing guitar since I was 15?
     alert('WRONG! How could you not know?');
   }
 }
+
 q2();
+
 function q3() {var answer3 = prompt('Do I have 2 cats?').toLowerCase();
   if (answer3 === 'yes' || answer3 === 'y') {
     alert('Correct! Well done!');
@@ -29,7 +33,9 @@ function q3() {var answer3 = prompt('Do I have 2 cats?').toLowerCase();
     alert('WRONG!');
   }
 }
+
 q3();
+
 function q4() {var answer4 = prompt('Do I hate the Sounders?').toLowerCase();
   if (answer4 === 'no' || answer4 === 'n') {
     alert('Correct! GO SOUNDERS!');
@@ -38,6 +44,7 @@ function q4() {var answer4 = prompt('Do I hate the Sounders?').toLowerCase();
     alert('WRONG!');
   }
 }
+
 q4();
 function q5() {var answer5 = prompt('Do I manage a night club?').toLowerCase();
   if (answer5 === 'yes' || answer5 === 'y') {
@@ -47,25 +54,30 @@ function q5() {var answer5 = prompt('Do I manage a night club?').toLowerCase();
     alert('WRONG!');
   }
 }
-q5();
-var numberGameGuesses = 0;
-while(numberGameGuesses < 4) {
-  var answer6 = parseInt(prompt('What is my favorite number?'));
-  if (answer6 === 7) {
-    alert('Correct! You are obviously strong with the force young padawan...');
-    break;
-    counter++;
-  } else if(answer6 < 7) {
-    alert('Wrong! You\'re answer is too high!');
-    numberGameGuesses++;
-  } else if(answer6 > 7) {
-    alert('You\'re answer is too low!');
-    numberGameGuesses++;
-  } else {
-    alert('Please enter a number!');
-    numberGameGuesses++;
+q5();*/
+
+function q6() {
+
+  var numberGameGuesses = 0;
+  while(numberGameGuesses < 4) {
+    var answer6 = parseInt(prompt('What is my favorite number?'));
+    if (answer6 === 7) {
+      alert('Correct! You are obviously strong with the force young padawan...');
+      break;
+      counter++;
+    } else if(answer6 > 7) {
+      alert('Wrong! You\'re answer is too high!');
+      numberGameGuesses++;
+    } else if(answer6 < 7) {
+      alert('You\'re answer is too low!');
+      numberGameGuesses++;
+    } else {
+      alert('Please enter a number!');
+      numberGameGuesses++;
+    }
   }
 }
+q6();
 //remeber you can move numberGameGuesses down one curly bracket so you don't have to repeat using it for each if else statement.
 
 var statesLivedIn = ['AZ', 'LA', 'NY'];
