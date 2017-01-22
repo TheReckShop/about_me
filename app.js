a93ace637479372535a5a7b69c8717102ac076ba
David Cole <thereckshop@mac.com>
2017-01-22 06:48:28 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict';
-var userName = false;
 
+var userName = false;
 for (userName = false; {('Greetings!!! What is your name?'){
+
 alert('Welcome ' + userName + ', glad you\'re here!');
 function intro() = for (var userName != true)
 alert('sorry bro try again!)
